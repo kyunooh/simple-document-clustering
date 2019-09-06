76803bbdb7d7c36201acc39ce94a04bf271bf882
Hyeon-Mook Jerry Choi <chm073@gmail.com>
2019-09-07 02:16:30 +0900

    Add koarean case

---
+++ tfidf_kmeans_clustering_with_scikit_korean.py
@@ -0,0 +1,67 @@
+import os
+import re
+import string
+from pprint import pprint
+
+documents = []
+
+dir_name = 'korean-documents'
+
+for filename in os.listdir(dir_name):
+    f = open(dir_name + os.sep + filename)
+    documents.append(f.read())
+    f.close()
+
+
+### sklearn
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.cluster import KMeans
+
+
+def preprocessing(text):
+    text = text.lower()
+    text = re.sub(r"[{}]".format(string.punctuation), " ", text)
+    return text
+
+
+tfidf_v = TfidfVectorizer(preprocessor=preprocessing, use_idf=True)
+
+
+tfidf = tfidf_v.fit_transform(documents)
+
+
+kmeans = KMeans(n_clusters=5).fit(tfidf)
+
+predicted = kmeans.predict(tfidf)
+
+
+final = {}
+count = 0
+for filename in os.listdir(dir_name):
+    final[filename] = predicted[count]
+    count += 1
+
+defined_cluster = {}
+
+for k, v in final.items():
+    if defined_cluster.get(v) is None:
+        defined_cluster[v] = []
+    defined_cluster.get(v).append(k)
+
+
+pprint(defined_cluster)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
